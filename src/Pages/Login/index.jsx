35723375c2147204c26f4bda579e28d42999a07d
leonardo moya fernandes <leonardomoya03@gmail.com>
2022-05-10 17:31:02 -0300

    build: Create: form login

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,5 +1,80 @@
+import { Container } from "./style";
+import Input from "../../Components/input";
+import { useHistory } from "react-router-dom";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 const Login = () => {
-  return <div>Login</div>;
+
+  const history = useHistory()
+
+  const handleNavegation = (path) =>(
+
+    history.push(path)
+  ) 
+  const formSchema = yup.object().shape({
+  email: yup.string().required("Email obrigatorio").email("Email inválido"),
+  password: yup
+        .string()
+        .required("Senha obrigatoria")
+        .matches(
+          /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+          "E necessario  ter pelo menos 8 caracteres,1 letra maiuscula e uma minuscula,um caractere especial e um numero"
+        ),
+        })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  const onSubmitFunction = ({  email, password }) => {
+    
+    const user = {email,password}
+    console.log(user);
+
+    history.push("/")
+  };
+
+  return (
+    <Container>
+      <header>
+      <picture>
+        <img src="#" alt="Logo GetSight" />
+      </picture>
+      </header>
+      <main>
+      <form onSubmit={handleSubmit(onSubmitFunction)}>
+        <Input
+          name="email"
+          label="email"
+          placeholder="Digite seu email"
+          register={register}
+          error={errors.email?.message}
+        />
+
+        <Input
+          name="password"
+          label="Senha"
+          type="password"
+          placeholder="Digite sua senha"
+          register={register}
+          error={errors.password?.message}
+        />
+
+        <button type="submit">Logar</button>
+      </form>
+
+        <div>
+          <p>Não tem uma conta? <span onClick={() => handleNavegation("/register")}>fazer cadastro</span></p>
+        </div>
+      </main>
+    </Container>
+  );
 };
 
 export default Login;
