d1cbccda7dfc29b3f4dee4fc43cc8868c3808efb
CaueRafael <caue_rrds@hotmail.com>
2022-05-11 21:34:59 -0300

    feat: Add estilos para pagina Login

--- src/Pages/Login/style.js
+++ src/Pages/Login/style.js
@@ -1,28 +1,96 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+export const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  padding: 1rem;
+  align-items: stretch;
+  text-align: center;
+  background-color: var(--background-primary);
+  .boxImg {
+    display: none;
+  }
 
+  @media (min-width: 1101px) {
+    display: flex;
+    padding: 0;
+    .boxImg {
+      display: block;
 
-export const  Container = styled.div`
+      img {
+        width: 60vw;
+        height: 100vh;
+      }
+    }
+  }
+`;
 
-text-align: left;
-display:flex;
-flex-direction: column;
-align-items: center;
-margin-top:200px;
-img{
-    width:350px;
-    height:70px;
+export const Content = styled.div`
+  display: flex;
+  row-gap: 20px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 400px;
+  margin: auto;
+
+  img {
+    width: 100px;
+    margin-top: 50px;
+  }
+`;
+
+const fadeIn = keyframes`
+from{
+    opacity: 0;  
 }
-main{
-    display:flex;
+to {
+    opacity: 100%;
+}
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  row-gap: 15px;
+  animation: ${fadeIn} 1s;
+  width: 100%;
+  background-color: var(--light-grey);
+  padding: 1rem;
+  border-radius: 4px;
+
+  form,
+  .boxCadastro {
+    display: flex;
     flex-direction: column;
-    align-items: center;
-}
-form{
-    height:50vh;
-    display:flex;
-    flex-direction:column;
-    justify-content: space-around;
-    align-items: center;
-}
+    row-gap: 15px;
+    width: 100%;
+    text-align: center;
+    font-size: 0.8rem;
+    color: #f3f3f3;
+    h1 {
+      margin: 25px 0;
+      color: #f3f3f3;
+      font-size: 1rem;
+    }
+    .label {
+      margin-bottom: 10px;
+    }
+    button {
+      width: 100%;
+      font-weight: 500;
+    }
+  }
 
-`
+  .boxCadastro {
+    color: var(--back);
+    span {
+      font-weight: 1000;
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+`;
