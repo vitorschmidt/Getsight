7a6d61e1fa57970d83f3a20ea46c6276279c76d3
Lucas Paulus <lucas.apaulus012@gmail.com>
2022-05-13 02:24:27 -0300

    feat: button changed for header

--- src/Components/Button/styles.js
+++ src/Components/Button/styles.js
@@ -1,11 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.button`
-  width:120px;
-  height: 40px;
+  width: ${(props)=> props.width ? props.width : "120px"};
+  max-width: ${(props)=> props.maxWidth};
+
+  height: ${(props)=> props.height ? props.height : "40px"};
+
   background-color: ${(props) => props.backGround};
   color: ${(props) => props.textColor};
-  border-radius: 4px;
+  border-radius: ${(props)=> props.borderRadius ? props.borderRadius : "4px"};
   :hover {
     background-color: ${(props) => props.backGroundHover};
   }
