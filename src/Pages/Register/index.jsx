9539455f5ee66597f137925cffa9b622a947a7f9
Vitor Schmidt <schmidt_vitor@hotmail.com>
2022-05-13 17:18:22 -0300

    fix: register error

--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -11,7 +11,7 @@ import banner from "../../Assets/img//banner.jpg";
 
 const Register = () => {
   const history = useHistory();
-  const handleNavegation = (path) => history.push(path);
+
   const formSchema = yup.object().shape({
     name: yup
       .string()
@@ -58,67 +58,65 @@ const Register = () => {
       <div className="boxImg">
         <img src={banner} alt="banner" />
       </div>
+
+      <picture>
+        <img src={Logo} alt="Logo GetSight" />
+      </picture>
       <FormContainer>
-        <picture>
-          <img src={Logo} alt="Logo GetSight" />
-        </picture>
+        <form onSubmit={handleSubmit(onSubmitFunction)}>
+          <Inputs
+            name="name"
+            label="Nome"
+            placeholder="Digite seu nome"
+            register={register}
+            error={errors.name?.message}
+          />
 
-        <FormContainer>
-          <form onSubmit={handleSubmit(onSubmitFunction)}>
-            <Inputs
-              name="name"
-              label="Nome"
-              placeholder="Digite seu nome"
-              register={register}
-              error={errors.name?.message}
-            />
+          <Inputs
+            name="email"
+            label="Email"
+            placeholder="Digite seu email"
+            register={register}
+            error={errors.email?.message}
+          />
 
-            <Inputs
-              name="email"
-              label="Email"
-              placeholder="Digite seu email"
-              register={register}
-              error={errors.email?.message}
-            />
+          <Inputs
+            name="cidade"
+            label="Cidade"
+            placeholder="Digite sua cidade"
+            register={register}
+            error={errors.cidade?.message}
+          />
 
-            <Inputs
-              name="cidade"
-              label="Cidade"
-              placeholder="Digite sua cidade"
-              register={register}
-              error={errors.cidade?.message}
-            />
+          <Inputs
+            name="password"
+            label="Senha"
+            type="password"
+            placeholder="Digite sua senha"
+            register={register}
+            error={errors.password?.message}
+          />
 
-            <Inputs
-              name="password"
-              label="Senha"
-              type="password"
-              placeholder="Digite sua senha"
-              register={register}
-              error={errors.password?.message}
-            />
+          <Inputs
+            name="confirmPassword"
+            type="password"
+            label="Confirmar senha"
+            placeholder="Digite seu email"
+            register={register}
+            error={errors.confirmPassword?.message}
+          />
 
-            <Inputs
-              name="confirmPassword"
-              type="password"
-              label="Confirmar senha"
-              placeholder="Digite seu email"
-              register={register}
-              error={errors.confirmPassword?.message}
-            />
-
-            <Button
-              type="submit"
-              backGround="teal"
-              textColor="white"
-              backGroundHover="red"
-            >
-              Cadastrar
-            </Button>
-          </form>
-        </FormContainer>
-        {/* <Button onClick={()=> handleNavegation("/home")}>Home</Button> */}
-
+          <Button
+            type="submit"
+            backGround="teal"
+            textColor="white"
+            backGroundHover="red"
+          >
+            Cadastrar
+          </Button>
+        </form>
+      </FormContainer>
+      {/* <Button onClick={()=> handleNavegation("/home")}>Home</Button> */}
     </Container>
   );
 };
