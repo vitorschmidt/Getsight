5398d20fb021427dea79f82ba0ba169cfcc8a0d6
Lucas Paulus <lucas.apaulus012@gmail.com>
2022-05-13 02:28:00 -0300

    build: test header in pages

--- src/Pages/Candidates/index.jsx
+++ src/Pages/Candidates/index.jsx
@@ -1,9 +1,17 @@
 import { useHistory } from "react-router-dom";
+import Header from "../../Components/Header";
+
+
+
 const Candidates = () => {
   const history = useHistory();
   const handleNavegation = (path) => history.push(path)
   return (
   <>
+  {/* coloquei a header aqui da versao não logada pra testar */}
+  <Header bg="#C2D8DA" height="130px" headerVersion="homeCandidates">
+    
+  </Header>
   <div>Candidates</div>
   <button onClick={()=> handleNavegation("/home")}>Home</button>
   </>
