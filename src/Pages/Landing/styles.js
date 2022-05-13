bb020babbf432790f3221482440250447c7ee0ce
Vinicius <vinicius.stmartins@gmail.com>
2022-05-13 12:07:48 -0300

    fix: styled chance img and mobile

--- src/Pages/Landing/styles.js
+++ src/Pages/Landing/styles.js
@@ -1,17 +1,27 @@
 import styled from "styled-components";
+import vote from "../../Assets/img/vote.jpg";
+import banner from "../../Assets/img/banner-desktop.png"
+import { createGlobalStyle } from "styled-components";
 
 
 export const  Container = styled.div`
+width:100vw;
 height:100vh;
+background-image:url(${vote});
+background-repeat:no-repeat;
+background-position:right;
 background-color:#47777B;
 color:white;
 display:flex;
 flex-direction: column;
 justify-content: space-evenly;
+
+
 .header{
     display:flex;
     align-items: center;
     justify-content: space-between;
+   
 }
 #logo{
     height:70px;
@@ -25,6 +35,7 @@ justify-content: space-evenly;
 .text{
     display:flex;
     justify-content: space-around;
+    
 }
 .indice{
     display:flex;
@@ -33,10 +44,11 @@ justify-content: space-evenly;
     width:40%
 }
 .indice h1{
-    font-size:50px;
+    font-size:40px;
 }
 .indice h3{
-    font-size:40px;
+    font-size:30px;
+    font-weight:400;
 }
 .indice p{
     font-size:20px;
@@ -91,18 +103,99 @@ justify-content: space-evenly;
     color:white;
     background-color:#00F40A;
 }
-.politico{
-    width:50%;
-    display:flex;
-   align-items:center;
-}
-.politico img{
-    border-radius:50%;
-    height:80%;
-    
-}
+
 a:hover{
     cursor: pointer;
 }
+@media(min-width :1102px){
+    
+    background-image:url(${banner});
+    display:flex;
+    background-size:100vh;
 
-`
+    .header{
+   margin-top:-80px;
+   
+    }
+   
+    .indice{
+        margin-right:600px;
+        gap:20px;
+        display:flex;
+        align-items: stretch;
+     
+    }
+    .buttons{
+    display:flex;
+    align-items:center;
+    width:150px;
+    height:50px;
+    font-size:15px;
+    background-color:#008080;
+    border-radius:4px;
+    margin-right:100px;
+    box-shadow: 0px 4px 5px 0px rgba(0,0,0,0.75);
+}
+    
+}
+
+@media (max-width: 1101px) {
+   background-size:100vw;
+   background-size:100vh;
+   background-size:cover;
+   background-position:center;
+  
+   
+.header{
+    flex-direction: column;
+}
+.indice{
+    display:flex;
+    align-items: center;
+    justify-content: space-evenly;
+    width:80%;
+    height:80vh;
+    background-color:#47777bad ;
+    box-shadow: 0px -1px 17px -8px rgba(0,0,0,0.75);
+    padding:15px;
+
+}
+#logo{
+    height:40px;
+}
+.buttons{
+    display:flex;
+    align-items:center;
+    width:90px;
+    height:30px;
+    font-size:15px;
+    background-color:#008080;
+    border-radius:4px;
+    box-shadow: 0px 4px 5px 0px rgba(0,0,0,0.75);
+}
+.indice h1{
+    font-size:18px;
+}
+.indice h3{
+    font-size:12px;
+    font-weight:400;
+}
+.indice p{
+    font-size:8px;
+}
+a{
+    font-size:10px;
+}
+
+.indice button{
+    width:150px;
+    height:30px;
+    font-size:12px;
+
+}
+
+
+  }
+  
+`;
+
