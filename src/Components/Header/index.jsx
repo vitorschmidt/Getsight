d585a495f8303c842be6fb142f29dbf60730792e
Lucas Paulus <lucas.apaulus012@gmail.com>
2022-05-14 20:10:05 -0300

    feat: Mobile Headers

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 
 import {ReactComponent as Logo} from "../img/logo.svg"
 import { Button } from "../Button";
-import {Container, LogoContainer, Headers, ColRight, Input, InputContainer, Description} from  "./style.js"
+import {Container, LogoContainer, Headers, ColRight, Input, InputContainer, ElectionType} from  "./style.js"
+
 import { useHistory } from "react-router-dom";
+ 
+import { HeaderMediaQueries } from "./HeaderMediaQueries";
 
 const Header = ({
     bg, 
@@ -14,9 +17,13 @@ const Header = ({
 
     const history = useHistory()
 
+ 
+    
     return (
         <Container bg={bg} height={height} >
+            <HeaderMediaQueries version={headerVersion}>
             {
+               
                 /*  versão não logada*/
                 headerVersion === "home" ?
                     <Headers>
@@ -27,36 +34,43 @@ const Header = ({
                         
                         <ColRight width="300px">
                             <Link to="/login">Login</Link>
-                            <Button onClick={()=> history.push("/register")} width="100%" maxWidth="200px" height="50px" 
+                            <Button onClick={()=> history.push("/register")} width="100%" maxWidth="200px" height="40px" 
                             backGround="#000000" textColor="#fff" borderRadius="20px">Cadastrar</Button>
                         </ColRight>
                         
-                         
                     </Headers>
                 
                 :
 
                 headerVersion === "homeCandidates" ?
-                <Headers position="relative">
+                <Headers >
                      <LogoContainer width="100%" height="30px" maxWidth="143px" position="absolute">
                             <Logo/>
                            
                     </LogoContainer>
+                   
+                        <InputContainer paddingLeft="40px" width="70%">
+                            <Input desktop placeholder="Pesquisar Candidato, categoria, partido, numero, cargo"/>
+                            <Input mobile placeholder="Pesquisar Candidato"/>
+                            <Button backGround="#22333B" textColor="#FFFFFF" width="100%"  
+                            maxWidth="160px" height="45px" borderRadius="0 8px 8px 0">PESQUISAR</Button>
+                        </InputContainer>    
 
-                    <InputContainer paddingLeft="40px" width="70%">
-                        <Input placeholder="Pesquisar Candidato, categoria, partido, numero, cargo"/>
-                        <Button backGround="#22333B" textColor="#FFFFFF" width="100%" 
-                        maxWidth="160px" height="45px" borderRadius="0 8px 8px 0">PESQUISAR</Button>
-                    </InputContainer>    
+                        <ColRight paddingRight="20px" width="50%"  >
+                            <ElectionType>
+                                <label>Tipo de Eleição</label>
+                                <select>
+                                    <option>Municipal</option>
+                                    <option>Estadual</option>
+                                </select>
+                            </ElectionType>
 
-                    <ColRight paddingRight="20px" width="50%"  >
-                        <Description>
-                            <p className="positionTop">Tipo de eleição</p>
-                            <p>Municipal</p>
-                        </Description>
-                        <Button onClick={()=> history.push("/login")} width="100%" maxWidth="200px" height="50px" 
-                            backGround="#000000" textColor="#fff" borderRadius="20px">LOGIN</Button>
-                    </ColRight>
+                            <Button onClick={()=> history.push("/login")} width="100%" maxWidth="200px" 
+                                backGround="#000000" textColor="#fff" borderRadius="20px">LOGIN</Button>
+                        </ColRight>
+
+                  
+                   
                 
                 </Headers>
                 /* versão não logada vai até aqui */
@@ -70,11 +84,11 @@ const Header = ({
 
                     </LogoContainer>
                     
-                    <ColRight width="350px" fontSize="22px">
+                    <ColRight width="350px" aTagFontSize="clamp(14px, 2vw, 18px)" pTagFontSize="clamp(16px, 3vw, 22px)">
                         {/* aqui será inserido o nome do usuário  */}
                         <p>{user}</p>
                         <Link to="/candidates">Candidatos</Link>
-                        <Button onClick={()=> history.push("/")} width="100%" maxWidth="200px" height="50px" 
+                        <Button onClick={()=> history.push("/")} width="100%" maxWidth="200px"
                         backGround="#000000" textColor="#fff" borderRadius="20px">Sair</Button>
                     </ColRight>
                 
@@ -85,26 +99,33 @@ const Header = ({
 
                 headerVersion === "dashboardCandidates" ?
                 <Headers>
-                 
+                    <Link to="/">SAIR</Link>
                     <InputContainer width="70%">
-                        <Input placeholder="Pesquisar Candidato, categoria, partido, numero, cargo"/>
-                        <Button backGround="#22333B" textColor="#FFFFFF" width="100%" 
-                        maxWidth="160px" height="45px" borderRadius="0 8px 8px 0">PESQUISAR</Button>
+                        <Input desktop placeholder="Pesquisar Candidato, categoria, partido, numero, cargo"/>
+                        <Input mobile placeholder="Pesquisar Candidato"/>
+                        <Button backGround="#22333B" textColor="#FFFFFF" width="100%"  height="45px"
+                        maxWidth="160px" borderRadius="0 8px 8px 0">PESQUISAR</Button>
                     </InputContainer> 
                 
                     <ColRight width="20%"  >
-                   
-                        <Button onClick={()=> history.push("/")} width="100%" maxWidth="100px" height="50px" 
+                            <ElectionType>
+                                <label>Tipo de Eleição</label>
+                                <select>
+                                    <option>Municipal</option>
+                                    <option>Estadual</option>
+                                </select>
+                            </ElectionType>
+                        <Button onClick={()=> history.push("/")} width="100%" maxWidth="100px" 
                             backGround="#000000" textColor="#fff" borderRadius="20px">SAIR</Button>
                     </ColRight>
                  </Headers>
                 :
 
-                /* versão vai até aqui */
+                /* versão logada vai até aqui */
                 ""
-        
-            }
             
+            }
+            </HeaderMediaQueries>
         </Container>
     )
 }
--- src/Components/Header/style.js
+++ src/Components/Header/style.js
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-
+import styled, {css} from "styled-components";
+ 
 
 export const Container = styled.div`
     display: flex;
@@ -7,14 +7,20 @@ export const Container = styled.div`
     justify-content: center;
     font-family : "Inter", sans-serif ;
     width: 100%;
- 
+    
     height: ${(props)=> props.height};
     background-color: ${(props)=> props.bg};
     padding: 0 25px;
-
+    position: relative;
+    
     button{
         padding: 0 15px;
-       
+        font-size: clamp(16px, 1.5vw, 18px);
+        min-width: 120px;
+    }
+    a{
+
+        font-size: clamp(12px, 2vw, 16px);
     }
 
 `
@@ -24,9 +30,10 @@ export const Headers = styled.header`
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
-    position: ${(props)=> props.position};
 
+ 
 `
+
 
 export const ColRight = styled.div`
     display: flex;
@@ -37,64 +44,113 @@ export const ColRight = styled.div`
     a{
         margin: 0 20px;
         color: #000000;
+        font-size: ${(props)=> props.aTagFontSize};
     }
     
     p{
-        font-size: ${(props)=> props.fontSize}
+        font-size: ${(props)=> props.pTagFontSize}
     }
 
+    button{
+        margin-left: 15px;
+    }
+    
 `
 export const LogoContainer = styled.div`
     display: flex;
     justify-content: center;
     position: ${(props)=> props.position};
-    bottom: 48px;
+    top: 4px;
     svg{
         width: ${(props)=> props.width};
         max-width: ${(props)=> props.maxWidth};
         height:  ${(props)=> props.height}
     }
    
-     
 `
 
+ 
 export const Input = styled.input`
+    display:  ${(props)=> props.desktop ? "block" : "none"};
     width: 100%;
     height: 45px;
      
     border: none;
     border-radius: 8px 0px 0px 8px;
     padding: 0 20px;
-    font-size: clamp(10px, 1.6vw, 18px);
-     
+    font-size: clamp(14px, 2vw, 18px);
+  
     
     &::placeholder{
         color: #808080;
         
     }
-`
 
+    @media (max-width: 1220px) {
+        display: ${(props)=> props.mobile ? "block" : "none"};
+    }
+
+`
+ 
 export const InputContainer = styled.div`
     width: ${(props)=> props.width};
     display: flex;
     max-width: 800px;
     padding-left: ${(props)=> props.paddingLeft};
+    margin-right: 15px;
+
+   
 
 `
-export const Description = styled.div`
+export const ElectionType = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    width: 100%;
-    padding:  0 40px;
-    font-size: 18px;
+    min-width: 160px;
+   
+    margin:  0 auto;
+    border-radius: 12px;
+    background-color:#CFE7E9;
+    padding: 5px 15px;
     
-    .positionTop{
-        position: absolute;
-        bottom: 48px;
-        
+;
+    color: #808080;
+    label{
+        width: 100%;
+        border-bottom: 2px dashed rgba(34, 51, 59, 0.1);
+		font-size: clamp(12px,1.6vw,18px);
+	
+		margin-bottom: 5px;
+        text-align: center;
     }
+
+    select{
+        background-color: #c2d8da;
+;
+        border: none;width: 100%;
+        border-radius: 5px;
+        font-size: 12px;
+        color: #808080;
+        padding: 0 5px;
+        font-family: "Inter", sans-serif;
+        &:focus{
+            color: black;
+        }
+    }
+
+
+     option{
+        background-color: #c2d8da;
+        color: #22333b;
+
+     }
+    
 `
 
+export const MediaQueries = styled.div`
+       
+   
 
+
+`
