27097cd5e5b752d9bf4c1bef04f6eb96d6b540cd
Vitor Schmidt <schmidt_vitor@hotmail.com>
2022-05-14 13:53:02 -0300

    fix: cola styles

--- src/Pages/Cola/index.jsx
+++ src/Pages/Cola/index.jsx
@@ -1,20 +1,25 @@
-import { useHistory } from "react-router-dom";
 import Header from "../../Components/Header";
 import ListCola from "../../Components/listCola";
+import { Button } from "../../Components/Button";
+import { Container } from "./styles";
+
 const Cola = () => {
-    const history = useHistory();
-    const handleNavegation = (path) => history.push(path);
-    return (
-    <>
-    
-    <Header bg="#C2D8DA" height="130px" headerVersion="cola">
-    </Header>
-    <div>Cola</div>
-    <button onClick={()=> handleNavegation("/home")}>Home</button>
-    <ListCola/>
-    </>
-    );
-  };
-  
-  export default Cola;
-  
+  return (
+    <Container>
+      <Header bg="#C2D8DA" height="130px" headerVersion="cola"></Header>
+      <ListCola />
+      <Button
+        width="100%"
+        maxWidth="200px"
+        height="40px"
+        backGround="#051B03"
+        textColor="#fff"
+        borderRadius="8px"
+      >
+        Gerar Cola
+      </Button>
+    </Container>
+  );
+};
+
+export default Cola;
