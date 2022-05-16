57a66922307d3c237849a48f63e3d7c6c0e1934f
Vitor Schmidt <schmidt_vitor@hotmail.com>
2022-05-16 10:32:24 -0300

    create: base dos posts, base do estilo dos posts

--- src/Components/PostCard/index.jsx
+++ src/Components/PostCard/index.jsx
@@ -1,8 +1,32 @@
+import { Container, Feed, Post, UserInfo } from "./style";
+import logo from "../../Assets/img/candidate.png";
+
 const PostCard = ({ post }) => {
+  const comments = post.comments;
+
   return (
-    <div>
-      <h2>{post.post}</h2>
-    </div>
+    <Container>
+      <UserInfo>
+        <img src={logo} alt="foto-perfil" />
+        <div>
+          <h2>nome</h2>
+          <p>Cidade - Estado</p>
+        </div>
+      </UserInfo>
+      <Post>
+        <h2>{post.title}</h2>
+        <h3>{post.post}</h3>
+      </Post>
+      {/* {comments.map((el, userId) => (
+        <>
+          <p key={userId}>{el.message}</p>
+          <span>{el.like}</span>
+        </>
+      ))} */}
+      <Feed>
+        <h2>{post.likePost}</h2>
+      </Feed>
+    </Container>
   );
 };
 
