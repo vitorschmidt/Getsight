45e98406cca410810905486b362ac0d2ef8fbd88
Vinicius <vinicius.stmartins@gmail.com>
2022-05-16 14:12:48 -0300

    build:add provider context noticias

---
+++ src/Components/CardNoticias/style.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: #bebcbc;
+  margin-bottom: 30px;
+  border-radius: 5px;
+  align-items: center;
+  justify-content: space-between;
+  width: 250px;
+  height: 80px;
+  box-shadow: 0px 0px 11px 0px rgba(89, 89, 89, 0.75);
+
+  img {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    padding: 5px;
+    margin-left: 10px;
+    box-shadow: 0px 0px 11px 0px rgba(89, 89, 89, 0.75);
+  }
+
+  @media (min-width: 1101px) {
+    width: 800px;
+
+    img {
+      margin-left: 50px;
+    }
+
+    h2,
+    p,
+    h3 {
+      margin-right: 50px;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-right: 10px;
+
+  h2 {
+    font-size: 18px;
+  }
+  h3 {
+    font-weight: 400;
+    font-size: 16px;
+    color: var(--placeholder-text);
+  }
+
+  p {
+    font-size: 20px;
+    font-weight: 700;
+    color: var(--red-2);
+  }
+`;
