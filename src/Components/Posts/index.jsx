77a7d3f098dac6dae8ac13c7a327c8e0ee794281
Vitor Schmidt <schmidt_vitor@hotmail.com>
2022-05-17 13:02:38 -0300

    fix: styles create post

--- src/Components/Posts/index.jsx
+++ src/Components/Posts/index.jsx
@@ -4,6 +4,7 @@ import { useHome } from "../../Providers/Home";
 import PostCard from "../PostCard";
 import { Perfil, Post } from "./style";
 import imgPerfil from "../../Assets/img/candidate.png";
+import { Button } from "../../Components/Button";
 
 const Posts = ({ authenticated }) => {
   const { posts, getPosts } = useHome();
@@ -24,6 +25,16 @@ const Posts = ({ authenticated }) => {
         <form>
           <input type="text" placeholder="Titulo da sua publicacao" />
           <textarea name="post" placeholder="Comecar publicacao"></textarea>
+          <Button
+            width="40%"
+            maxWidth="200px"
+            height="30px"
+            backGround="#051B03"
+            textColor="#fff"
+            borderRadius="20px"
+          >
+            Postar
+          </Button>
         </form>
       </Post>
       {posts.map((post, index) => (
