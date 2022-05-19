bf0ad4d7af3fbc7cdf0b971cdb6d8a5c593013a6
Vitor Schmidt <schmidt_vitor@hotmail.com>
2022-05-19 17:46:07 -0300

    fix: margin menu posts/noticias

--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -60,6 +60,7 @@ export const Menu = styled.div`
   background-color: var(--light-grey);
   box-shadow: 0px 0px 11px 0px rgba(89, 89, 89, 0.75);
   border-radius: 8px;
+  margin-bottom: 15px;
   button {
     margin: 20px;
   }
