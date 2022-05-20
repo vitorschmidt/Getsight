1449995b2d82b81a1224c581f06f9eb2928b4ed9
Vitor Schmidt <schmidt_vitor@hotmail.com>
2022-05-19 21:33:37 -0300

    fix: final fix

--- src/Pages/Cola/index.jsx
+++ src/Pages/Cola/index.jsx
@@ -4,7 +4,6 @@ import { Container } from "./styles";
 //Components imports
 import Header from "../../Components/Header";
 import ListCola from "../../Components/listCola";
-import { toast, Toaster } from "react-hot-toast";
 
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
@@ -12,20 +11,11 @@ const Cola = ({ authenticated }) => {
   const history = useHistory();
 
   if (!authenticated) {
-    toast.error("Logar para ver a cola!", {
-      style: {
-        borderRadius: "10px",
-        background: "#333",
-        color: "#fff",
-      },
-    });
-
     history.push("/home");
   }
 
   return (
     <Container>
-      <Toaster />
       <Header bg="#C2D8DA" height="130px" headerVersion="cola"></Header>
       <ListCola />
     </Container>
